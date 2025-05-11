0bf9efa55eb3918f22805454558484505eaa0481
RuukRey1412 <seijiwork1412@gmail.com>
2025-05-12 04:14:40 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,32 +1,131 @@
 const actresses = [
     {
-        name: "三上悠亜",
-        genre: "グラビア, AV女優",
-        img: "https://via.placeholder.com/200x200?text=三上悠亜",
-        link: "https://example.com/mikami"
+        name: "JD",
+        link: "https://x.com/sano_xxxxxx/status/1638519255489708036"
     },
-    {
-        name: "高橋しょう子",
-        genre: "AV女優",
-        img: "https://via.placeholder.com/200x200?text=高橋しょう子",
-        link: "https://example.com/takahashi"
+
+{
+        name: "JD",
+        link: "https://x.com/inumakitokyo/status/1905183020552004065"
+    },{
+        name: "JD",
+        link: "https://x.com/y_ofpako/status/1116321173900419073"
+    },{
+        name: "JD",
+        link: "https://x.com/y_ofpako/status/1118132520732758017"
+    },{
+        name: "JD",
+        link: "https://x.com/y_ofpako/status/1118857180180959232"
+    },{
+        name: "JD",
+        link: "https://x.com/inumakitokyo/status/1861412935450669320"
+    },{
+        name: "JD",
+        link: "https://x.com/ken_ura18/status/1173233514646368258"
+    },{
+        name: "JD",
+        link: "https://x.com/inumakitokyo/status/1850497118353690805"
+    },{
+        name: "JD",
+        link: "https://x.com/torunodouga/status/1467691649203838976"
     },
-    {
-        name: "河北彩花",
-        genre: "AV女優",
-        img: "https://via.placeholder.com/200x200?text=河北彩花",
-        link: "https://example.com/kahoku"
+{
+        name: "JD",
+        link: "https://x.com/inumakitokyo/status/1897446101701243351"
     },
-    {
-        name: "明日花キララ",
-        genre: "グラビア, AV女優",
-        img: "https://via.placeholder.com/200x200?text=明日花キララ",
-        link: "https://example.com/ashitahana"
+{
+        name: "JD",
+        link: "https://x.com/y_ofpako/status/1048555209360732161"
     },
-    {
-        name: "橋本ありな",
-        genre: "AV女優",
-        img: "https://via.placeholder.com/200x200?text=橋本ありな",
-        link: "https://example.com/hashimoto"
-    }
-];
+{
+        name: "JD",
+        link: "https://x.com/WPaco20781/status/1173395384875585537"
+    },
+{
+        name: "JD",
+        link: "https://x.com/y_ofpako/status/1049646291741958145"
+    },
+{
+        name: "JD",
+        link: "https://x.com/sirouto5439/status/1804461940582101132"
+    },
+{
+        name: "JD",
+        link: "https://x.com/taku_eroaffili/status/1556973974374027265"
+    },
+{
+        name: "JD",
+        link: "https://x.com/naoki_groper2/status/1573154622247964672"
+    },
+{
+        name: "JD",
+        link: "https://x.com/shun398/status/1290624936373915650"
+    },
+{
+        name: "JD",
+        link: "https://x.com/skiyaki_69/status/1216679027252940800"
+    },
+{
+        name: "JD",
+        link: "https://x.com/ayaaya_jdo55/status/1125682949159104512"
+    },
+{
+        name: "JD",
+        link: "https://x.com/fna56Eg27Kx/status/1601116216818806785"
+    },
+{
+        name: "JD",
+        link: "https://x.com/hentaidanyuu/status/1668937979287711745"
+    },
+{
+        name: "JD",
+        link: "https://x.com/i_h_uraXXX/status/1809185876699746544"
+    },
+{
+        name: "JD",
+        link: "https://x.com/i_hxxayokx3xX/status/1921569178386907347"
+    },{
+        name: "JD",
+        link: "https://x.com/PsychoVincii/status/1921541007243370849"
+    },
+{
+        name: "JD",
+        link: "https://x.com/Lemon_vD/status/1593947950924824584"
+    },
+{
+        name: "JD",
+        link: "https://x.com/shinosuke_bee/status/1919741014786638009"
+    },
+{
+        name: "JD",
+        link: "https://x.com/shinosuke_bee/status/1863912287255642328"
+    },
+{
+        name: "JD",
+        link: "https://x.com/hentaidanyuu/status/1720478316434698747"
+    },
+{
+        name: "JD",
+        link: "https://x.com/hentaidanyuu/status/1757380244208857592"
+    },
+{
+        name: "JD",
+        link: "https://x.com/reon__xxrp/status/1891812712411459608"
+    },{
+        name: "JD",
+        link: "https://x.com/asobinin_xxx1/status/1880137845656223766"
+    },
+{
+        name: "JD",
+        link: "https://x.com/boku_to_tamago3/status/1808457681868574871"
+    },
+{
+        name: "JD",
+        link: "https://x.com/tam_sub00/status/1846128893687628090"
+    },
+{
+        name: "JD",
+        link: "https://x.com/yuriyuri020202U/status/1839636752648306918"
+    },
+
+]
